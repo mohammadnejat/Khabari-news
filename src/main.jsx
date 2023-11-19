9e65ce1f00e032d9b7438c8c880e3afde44c513d
mohammad <mohammadofahmad@gmail.com>
2023-11-19 20:57:01 +0330

    fixed bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <App/>
 )
