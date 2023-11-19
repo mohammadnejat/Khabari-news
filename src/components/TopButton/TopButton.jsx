398c261805c3de6ae37da51ec01dca12bf63dc6f
mohammad <mohammadofahmad@gmail.com>
2023-11-19 20:48:57 +0330

    some new featchers

--- src/components/TopButton/TopButton.jsx
+++ src/components/TopButton/TopButton.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import { AiOutlineArrowUp } from "react-icons/ai"
-import Fade from 'react-reveal/Fade';
+import "./TopButton.css"
 const TopButton = () => {
     const [showButton, setShowButton] = useState(false);
 
     const handleScroll = () => {
-        if (window.scrollY > 300) {
+        if (window.scrollY >= 300) {
             setShowButton(true);
         } else {
             setShowButton(false);
@@ -17,16 +17,11 @@ const TopButton = () => {
     window.addEventListener('scroll', handleScroll);
     return (
         <>
-            {window.scrollY > 300 ? (
-                <Fade bottom>
-                    <div className={`fixed p-2 transition duration-500 bg-black rounded-full cursor-pointer hover:bg-red-700 bottom-10 left-6  scroll-to-top ${showButton ? 'show' : 'hide'}`} onClick={ScrollTop}>
+                    <div className={`fixed p-2 transition duration-500 bg-black rounded-full  cursor-pointer hover:bg-red-700 bottom-10 left-6  scroll-to-top ${showButton ? 'show' : 'hidee'}`} onClick={ScrollTop}>
                         <div className=''>
                             <AiOutlineArrowUp className='text-3xl text-white' />
                         </div>
                     </div>
-                </Fade>
-            ) : ""}
-
         </>
     )
 }
